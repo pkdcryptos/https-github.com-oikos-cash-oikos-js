4e79cc7e4ec739ce0e1044fafe214209beb2911f
Kevin Holder <kevholderx@gmail.com>
2020-04-12 15:36:01 -0400

    repl: Allow top level await

--- repl.js
+++ repl.js
@@ -1,4 +1,5 @@
 require('@babel/register');
+const BigNumber = require('BigNumber.js');
 
 const repl = require('repl');
 
@@ -6,10 +7,17 @@ const { SynthetixJs } = require('./src/index.node');
 const snxjs = new SynthetixJs({ networkId: 2 });
 snxjs.contractSettings.tronWeb.setPrivateKey(process.env.PRIVATE_KEY);
 
+const bn = o => BigNumber(o._hex).toString();
+
 // module.exports = snxjs;
 
-console.log('SynthetixJs is available from the snx variable.');
+console.log('SynthetixJs is available from the snx variable. More utils: ctx');
 console.log('Use the PRIVATE_KEY environment variable to set the private key.');
 const context = repl.start('> ').context;
-context.snx = snxjs;
-context.b32 = snxjs.ethers.utils.formatBytes32String;
+const ctx = {
+  snx: snxjs,
+  BigNumber,
+  b32: snxjs.ethers.utils.formatBytes32String,
+  bn,
+};
+Object.assign(context, ctx);
